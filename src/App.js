14e072aaf330b218fd2ce688e1f3408f5b2d06d7
hamzcheeze <ham_anurak@hotmail.com>
2023-10-19 11:20:42 +0700

    Initial Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+import Quotation from "./pages/quotation/Quotation"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Quotation />} />
+          {/* <Route path="test" element={<Test />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
